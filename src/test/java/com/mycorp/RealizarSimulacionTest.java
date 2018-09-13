0108281e70ee213a97d1bd2997d34513cc543686
Luis Arroyo Gonzalez <larroyogonzalez@gmail.com>
2018-09-13 21:53:38 +0200

    Correcion de tests

--- src/test/java/com/mycorp/RealizarSimulacionTest.java
+++ src/test/java/com/mycorp/RealizarSimulacionTest.java
@@ -3,73 +3,61 @@ package com.mycorp;
 import org.junit.Test;
 
 import com.mycorp.support.Ticket;
-import com.ning.http.client.AsyncHttpClient;
 
 import junit.framework.TestCase;
-
 
 /**
  * Unit test for simple App.
  */
 public class RealizarSimulacionTest extends TestCase {
 
-	private AsyncHttpClient client;
-	private String url = "";
-	private String username = "";
-	private String password = "";
-	private String token = "";
-	
-    /**
-     * Rigourous Test :-)
-     */
-    @Test
-    public void testApp() {
-        assertTrue( true );
-    }
+	private String url = "url";
+	private String username = "username";
+	private String password = "passw";
+	private String token = "token";
 
-    @Test
-    public void testZendesk() {
-    	Zendesk zendesk = null;
-    	assertNull(zendesk);
-    }
-    @Test
-    public void testSetUsername() {
-    	Builder builder = new Builder("");
-    	builder.setUsername(username);
-        assertEquals(username, builder.getUsername());
-    }
+	@Test
+	public void testZendesk() {
+		Zendesk zendesk = null;
+		assertNull(zendesk);
+	}
 
-    @Test
-    public void testSetPassword() {    
-    	Builder builder = new Builder("");
-    	builder.setPassword(password);
-        assertEquals(password, builder.getPassword());
-    }
+	@Test
+	public void testSetUsername() {
+		Builder builder = new Builder(url);
+		builder.setUsername(username);
+		assertEquals(username, builder.getUsername());
+	}
 
-    @Test
-    public void testSetToken() {
-    	Builder builder = new Builder("");
-    	builder.setToken(token);
-        assertEquals(token, builder.getToken());
-    }
+	@Test
+	public void testSetPassword() {
+		Builder builder = new Builder(url);
+		builder.setPassword(password);
+		assertEquals(password, builder.getPassword());
+	}
 
-    @Test
-    public void testBuild() {
-    	Builder builder = new Builder("");
-    	Zendesk zendesk = builder.setUsername(username).setPassword(password).setToken(token).build();
-    	
-        assertNotNull(zendesk);
-                
-        zendesk = builder.setUsername(username).setPassword(password).setToken(null).build();
-        assertNotNull(zendesk);
-    }
-    
-    @Test 
-    public void testCreateTicket() {
-    	Builder builder = new Builder("");
-    	Zendesk zendesk = builder.setUsername(username).setPassword(password).setToken(token).build();
-    	zendesk.createTicket(new Ticket());
-    	assertTrue(true);
-    }
-    
+	@Test
+	public void testSetToken() {
+		Builder builder = new Builder(url);
+		builder.setToken(token);
+		assertEquals(token, builder.getToken());
+	}
+
+	@Test
+	public void testBuild() {
+		Builder builder = new Builder(url);
+		Zendesk zendesk = builder.setUsername(username).setPassword(password).setToken(token).build();
+		assertNotNull(zendesk);
+		zendesk = builder.setUsername(username).setPassword(password).setToken(null).build();
+		assertNotNull(zendesk);
+	}
+
+	@Test
+	public void testCreateTicket() {
+		Builder builder = new Builder(url);
+		Zendesk zendesk = builder.setUsername(username).setPassword(password).setToken(token).build();
+		Ticket ticket = zendesk.createTicket(new Ticket());
+		assertNotNull(ticket);
+	}
+
 }
--- src/test/java/com/mycorp/ZendeskServiceTest.java
+++ src/test/java/com/mycorp/ZendeskServiceTest.java
@@ -7,36 +7,36 @@ import util.datos.UsuarioAlta;
 
 public class ZendeskServiceTest extends TestCase {
 
-	// TODO: explicar esto
-	@Test(expected = NumberFormatException.class)
+	@Test
 	public void testAltaTicketZendesk() {
 		ZendeskService zenService = new ZendeskService();
 		UsuarioAlta usuarioAlta = new UsuarioAlta();
 		String userAgent = "correo@mail.com";
-		zenService.altaTicketZendesk(usuarioAlta, userAgent);
+		String out = zenService.altaTicketZendesk(usuarioAlta, userAgent);
+		assertNotNull(out);
 	}
 
-	// TODO: explicar esto
-	@Test(expected = NumberFormatException.class)
+	@Test
 	public void testAltaTicketZendeskConNumTarjeta() {
 		ZendeskService zenService = new ZendeskService();
 		UsuarioAlta usuarioAlta = new UsuarioAlta();
 		usuarioAlta.setEmail("correo@mail.com");
 		String userAgent = "correo@mail.com";
 		usuarioAlta.setNumTarjeta("123");
-		zenService.altaTicketZendesk(usuarioAlta, userAgent);
+		String out = zenService.altaTicketZendesk(usuarioAlta, userAgent);
+		assertNotNull(out);
 	}
 
-	// TODO: explicar esto
-	@Test(expected = NumberFormatException.class)
+	@Test
 	public void testAltaTicketZendeskConNumPoliza() {
 		ZendeskService zenService = new ZendeskService();
 		UsuarioAlta usuarioAlta = new UsuarioAlta();
 		usuarioAlta.setEmail("correo@mail.com");
-		String userAgent = "correo@mail.com";		
+		String userAgent = "correo@mail.com";
 		usuarioAlta.setNumPoliza("1234");
-		usuarioAlta.setNumDocAcreditativo("1234");        
-		zenService.altaTicketZendesk(usuarioAlta, userAgent);
+		usuarioAlta.setNumDocAcreditativo("1234");
+		String out = zenService.altaTicketZendesk(usuarioAlta, userAgent);
+		assertNotNull(out);
 	}
 
 }
